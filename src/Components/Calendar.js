7a7975e5ef4bb364e0cd93efa3187f27ed3837df
unknown <popacezar96@yahoo.com>
2021-10-05 13:54:45 +0300

    Calendar styling and functionality

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,13 +1,93 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+
+const date = new Date();
+
+const renderCalendar = () => {
+  date.setDate(1);
+
+  const monthDays = document.querySelector(".days");
+
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = 7 - lastDayIndex - 1;
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+  document.querySelector(".date p").innerHTML = date.getFullYear();
+
+  let days = "";
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class="selected-day">${i}</div>`;
+    } else {
+      days += `<div>${i}</div>`;
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
+};
+
+// calling the function initially
+/*
+window.onload = function (){
+}*/
 
 const Calendar = ()=>{
+
+  useEffect(()=>{
+    renderCalendar();
+  });
+
   return(
       <div className="calendar">
         <div className="month">
 
           <div className="date">
-            <h1>September</h1>
-            <p className="year-count">2021</p>
+            <h1></h1>
+            <p></p>
           </div>
 
         </div>
