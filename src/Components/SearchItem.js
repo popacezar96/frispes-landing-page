d68fcd0276d9241b7be6bfc3605a79dd66201031
unknown <popacezar96@yahoo.com>
2021-10-14 16:14:09 +0300

    dropdown arrows as imgs

--- src/Components/SearchItem.js
+++ src/Components/SearchItem.js
@@ -1,5 +1,5 @@
 import {React} from 'react';
-
+import dropdown from '../dropdown-icon.svg';
 const SearchItem = (props)=>{
 
   const title = props.title;
@@ -9,6 +9,7 @@ const SearchItem = (props)=>{
       <button className='search-item'>
         <span>{title}</span>
         <p>{content}</p>
+        <img src={dropdown} className='dropdown-arrow' alt="D"/>
       </button>
   );
 };
